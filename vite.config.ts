6ad2363558013ef7988252dfb36efa9fc0751862
Mysterken <Mysterkenzo@gmail.com>
2023-11-05 21:10:41 +0100

    add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from "vite-plugin-vuetify";
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ankiCardCreator/',
-    plugins: [vue(), vuetify({autoImport: true})],
+    // append with /ankiCardCreator/ only in production
+    base: process.env.NODE_ENV === 'production' ? '/ankiCardCreator/' : '/',
+    plugins: [
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true,
+            },
+        }),
+        vue(),
+        vuetify({autoImport: true})
+    ],
     optimizeDeps: {
         include: ['lodash-es', 'isomorphic-dompurify'], // Add Lodash-ES to the list of dependencies to include
     },
