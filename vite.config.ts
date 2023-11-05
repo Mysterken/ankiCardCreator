196b112c34d0bedefa2d4c55ceba62dc9dd6f9d5
Mysterken <Mysterkenzo@gmail.com>
2023-11-05 20:17:58 +0100

    config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/ankiCardCreator/',
     plugins: [vue(), vuetify({autoImport: true})],
     optimizeDeps: {
         include: ['lodash-es', 'isomorphic-dompurify'], // Add Lodash-ES to the list of dependencies to include
